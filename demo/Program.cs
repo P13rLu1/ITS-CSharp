353d480d7357e962aa8f193c22042605909b7402
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-03-27 11:06:04 +0100

    aggiornato con esercizi fatti

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace demo;
 
-Console.WriteLine("Ciao Bello");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // int numero = 10;
+        // decimal nDecimale = 10.5M;
+        // string parola = "Ciao";
+        // bool vero = true;
+
+        Console.WriteLine("Inserisci una parola");
+        var digitato = Console.ReadLine() ?? "Sconosciuto"; // null coalescing operator
+        /* if (String.IsNullOrEmpty(digitato))
+        {
+            Console.WriteLine("Non hai inserito nulla");
+        }
+        else
+        {
+            Console.WriteLine("Hai inserito: " + digitato);
+        } */
+        // Console.Write("Ehi, ");
+        // Console.WriteLine("hai inserito: " + digitato);
+        Console.WriteLine($"Hai inserito: {digitato}"); // interpolazione di stringhe
+        var nomeMaiuscolo = digitato.ToUpper(); // inferenza di tipo, funziona solo se ha un valore
+        Console.WriteLine(nomeMaiuscolo);
+        string[] nomi =
+        {
+            "Mario",
+            "Luigi",
+            "Peach",
+            "Toad"
+        }; // array di stringhe, la lunghezza è fissa
+
+        if (nomi.Contains("Mario")) Console.WriteLine("C'è");
+
+        foreach (var nome in nomi)
+        {
+            Console.WriteLine(nome);
+            foreach (var lettera in nome) Console.WriteLine(lettera);
+        }
+
+        var num = 34.536M;
+        Console.WriteLine(num.ToString("N2")); // customizzare formato valore numerico
+        Console.WriteLine(5 / 10);
+    }
+}
--- esercizio1/Program.cs
+++ esercizio1/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿namespace esercizio1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        decimal costoProdotto;
+        decimal importoPagato;
+        decimal resto;
+
+        Console.Write("Inserisci il costo del prodotto acquistato: ");
+        if (!decimal.TryParse(Console.ReadLine(), out costoProdotto) || costoProdotto < 0)
+        {
+            Console.WriteLine("\nIl costo del prodotto inserito non è valido.");
+            return;
+        }
+
+        Console.Write("Inserisci l'importo pagato dal cliente: ");
+        if (!decimal.TryParse(Console.ReadLine(), out importoPagato) || importoPagato < 0)
+        {
+            Console.WriteLine("\nL'importo pagato inserito non è valido.");
+            return;
+        }
+
+        if (importoPagato < costoProdotto)
+        {
+            Console.WriteLine("L'importo pagato è inferiore al costo del prodotto.");
+            return;
+        }
+
+        resto = importoPagato - costoProdotto;
+
+        Console.WriteLine($"Il resto da dare al cliente è: {resto}");
+    }
+}
