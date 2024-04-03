8770a6f75f67c44515efdddd11533d754ee9ee0d
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-04-03 09:57:43 +0200

    optimized code

--- esercizio2/Program.cs
+++ esercizio2/Program.cs
@@ -1,21 +1,15 @@
-﻿//Creare una .NET Console App che simuli le funzionalità di base di una calcolatrice. Al lancio, l'applicazione presenterà all'utente un menu di selezione per il tipo di operazione matematica desiderata: addizione, sottrazione, moltiplicazione, divisione, o l'opzione per terminare l'esecuzione del programma. Una volta selezionata l'operazione, l'applicazione richiederà all'utente di inserire due valori numerici. Dopo l'inserimento, calcolerà e visualizzerà il risultato dell'operazione scelta. L'applicazione poi riproporrà il menu per permettere all'utente di eseguire ulteriori calcoli o di terminare il programma. Durante l'interazione, l'applicazione gestirà eventuali errori di input, assicurando che l'utente possa inserire solo numeri validi e prevenendo il crash del programma in caso di errori di digitazione.
-
-namespace esercizio2
+﻿namespace esercizio2
 {
     static class Program
     {
         static void Main()
         {
-            bool continua = true;
-
-            while (continua)
+            while (true)
             {
                 Console.Write(
                     "Seleziona l'operazione desiderata:\n1. Addizione\n2. Sottrazione\n3. Moltiplicazione\n4. Divisione\n5. Esci\nScelta: ");
 
-                string? sceltaOperazione = Console.ReadLine();
-
-                switch (sceltaOperazione)
+                switch (Console.ReadLine())
                 {
                     case "1":
                         EseguiOperazione('+');
@@ -30,11 +24,10 @@ namespace esercizio2
                         EseguiOperazione('/');
                         break;
                     case "5":
-                        continua = false;
-                        break;
+                        return;
                     default:
-                        Console.WriteLine("\nScelta non valida. Riprova. (Coglione)");
-                        Console.Write("Premi un tasto per continuare...");
+                        Console.WriteLine("\nScelta non valida. Riprova.");
+                        Console.Write("Premi un tasto per continuare..."); // Attesa di un input per continuare
                         Console.ReadKey();
                         break;
                 }
@@ -43,20 +36,16 @@ namespace esercizio2
 
         static void EseguiOperazione(char operatore)
         {
-            double num1, num2;
+            double[] numeri = new double[2];
 
-            Console.Write("Inserisci il primo numero: ");
-            while (!double.TryParse(Console.ReadLine(), out num1))
+            for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine("\nInput non valido. Inserisci un numero valido.");
-                Console.Write("Primo numero: ");
-            }
-
-            Console.Write("Inserisci il secondo numero: ");
-            while (!double.TryParse(Console.ReadLine(), out num2))
-            {
-                Console.WriteLine("\nInput non valido. Inserisci un numero valido.");
-                Console.Write("Secondo numero: ");
+                Console.Write($"Inserisci il {(i == 0 ? "primo" : "secondo")} numero: ");
+                while (!double.TryParse(Console.ReadLine(), out numeri[i])) // Ripeti finché l'input non è un numero
+                {
+                    Console.WriteLine("\nInput non valido. Inserisci un numero valido.");
+                    Console.Write($"{(i == 0 ? "Primo" : "Secondo")} numero: ");
+                }
             }
 
             double risultato;
@@ -64,18 +53,18 @@ namespace esercizio2
             switch (operatore)
             {
                 case '+':
-                    risultato = num1 + num2;
+                    risultato = numeri[0] + numeri[1];
                     break;
                 case '-':
-                    risultato = num1 - num2;
+                    risultato = numeri[0] - numeri[1];
                     break;
                 case '*':
-                    risultato = num1 * num2;
+                    risultato = numeri[0] * numeri[1];
                     break;
                 case '/':
-                    if (num2 != 0)
+                    if (numeri[1] != 0) // Controllo per evitare la divisione per zero
                     {
-                        risultato = num1 / num2;
+                        risultato = numeri[0] / numeri[1];
                     }
                     else
                     {
@@ -91,9 +80,9 @@ namespace esercizio2
                     return;
             }
 
-            Console.WriteLine($"\nRisultato: {risultato}");
+            Console.WriteLine($"\nRisultato: {risultato:N2}"); // Visualizzazione del risultato con due cifre decimali
             Console.Write("Premi un tasto per continuare...");
-            Console.ReadKey(); // Attende che l'utente prema un tasto
+            Console.ReadKey();
         }
     }
 }
