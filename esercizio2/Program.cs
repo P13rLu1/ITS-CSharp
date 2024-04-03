618860ff8f9447bc391776fd8c42dc702c9794a6
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-04-03 13:12:11 +0200

    put comments

--- esercizio2/Program.cs
+++ esercizio2/Program.cs
@@ -9,25 +9,25 @@ namespace esercizio2
             while (true)
             {
                 Console.Write(
-                    "Seleziona l'operazione desiderata:\n1. Addizione\n2. Sottrazione\n3. Moltiplicazione\n4. Divisione\n5. Esci\nScelta: ");
+                    "Seleziona l'operazione desiderata:\n1. Addizione\n2. Sottrazione\n3. Moltiplicazione\n4. Divisione\n5. Esci\nScelta: "); // Menu di selezione
 
                 switch (Console.ReadLine())
                 {
-                    case "1":
-                        EseguiOperazione('+');
+                    case "1": 
+                        EseguiOperazione('+'); // Chiamata alla funzione per eseguire l'operazione di addizione
                         break;
                     case "2":
-                        EseguiOperazione('-');
+                        EseguiOperazione('-'); // Chiamata alla funzione per eseguire l'operazione di sottrazione
                         break;
                     case "3":
-                        EseguiOperazione('*');
+                        EseguiOperazione('*'); // Chiamata alla funzione per eseguire l'operazione di moltiplicazione
                         break;
                     case "4":
-                        EseguiOperazione('/');
+                        EseguiOperazione('/'); // Chiamata alla funzione per eseguire l'operazione di divisione
                         break;
-                    case "5":
-                        return;
-                    default:
+                    case "5": 
+                        return; // Uscita dal programma
+                    default: // Caso di scelta non valida
                         Console.WriteLine("\nScelta non valida. Riprova.");
                         Console.Write("Premi un tasto per continuare..."); // Attesa di un input per continuare
                         Console.ReadKey();
@@ -36,9 +36,9 @@ namespace esercizio2
             }
         }
 
-        static void EseguiOperazione(char operatore)
+        private static void EseguiOperazione(char operatore) // Funzione per eseguire le operazioni matematiche
         {
-            var numeri = new double[2];
+            var numeri = new double[2]; // Array per contenere i due numeri inseriti dall'utente
 
             for (var i = 0; i < 2; i++)
             {
@@ -54,8 +54,8 @@ namespace esercizio2
 
             switch (operatore)
             {
-                case '+':
-                    risultato = numeri[0] + numeri[1];
+                case '+': // Calcolo del risultato in base all'operatore scelto
+                    risultato = numeri[0] + numeri[1]; 
                     break;
                 case '-':
                     risultato = numeri[0] - numeri[1];
